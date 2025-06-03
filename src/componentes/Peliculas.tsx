f651fcfb66af7989524c44597832f788b991219b
lxstik <egorrfal32@gmail.com>
2025-06-03 19:36:53 +0200

    coreccion 4, definir la interfaz

--- src/componentes/Peliculas.tsx
+++ src/componentes/Peliculas.tsx
@@ -1,45 +1,50 @@
-//el componente se ejecuta en el cliente, para poder usar hooks
 "use client";
 
 import { useEffect, useState } from "react";
 
+//defino la interfaz de pelicula
+interface Movie {
+  id: number;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+}
+
 export default function Peliculas() {
   //defino el estado de las peliculas para que sea un array vacío al principio, luego se llenará con las peliculas de la API
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
 
   //utilizo useEffect para que se ejecute nada más al cargar el componente
   useEffect(() => {
-  const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-  const BASE_URL = "https://api.themoviedb.org/3";
+    const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+    const BASE_URL = "https://api.themoviedb.org/3";
 
-  //hago la petición a la API obteniendo las peliculas populares
-  fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
-    .then(function(respuesta) {
-      //al obtener la respuesta la convierto a JSON para poder trabajar con esos datos
-      return respuesta.json();
-    })
-    .then(function(datos) {
-      // guardo los resultados en el estado
-      setMovies(datos.results);
-    });
-}, []);
+    //hago la petición a la API obteniendo las peliculas populares
+    fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+      .then(function (respuesta) {
+        //al obtener la respuesta la convierto a JSON para poder trabajar con esos datos
+        return respuesta.json();
+      })
+      .then(function (datos) {
+        // guardo los resultados en el estado
+        setMovies(datos.results);
+      });
+  }, []);
 
-  
-   return (
+  return (
     <div className="w-full">
-      <div className="grid grid-cols-3 gap]">
+      <div className="grid grid-cols-3">
         {movies.map((movie) => (
-          <div key={movie.id} style={{padding: "0px 0px 20px 0px"}}>
+          <div key={movie.id} style={{ padding: "0px 0px 20px 0px" }}>
             <img
-            //utilizo la url de la imagen de la api de la pelicula, lo mismo saco con los datos de votos y nombre
+              //utilizo la url de la imagen de la api de la pelicula, lo mismo saco con los datos de votos y nombre
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
               className="w-full h-100 object-cover"
             />
             <div className="font-bold">
               <p>{movie.title}</p>
-              <p>
-                Valoración:⭐ {movie.vote_average} / 10
-              </p>
+              <p>Valoración:⭐ {movie.vote_average} / 10</p>
             </div>
           </div>
         ))}
